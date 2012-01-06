90ab9b232db3d1e3ba2e51d439af0f4347faaff5
Trevor Kimenye <trevor@kimenye.com>
2012-01-06 13:30:20 +0300

    changed port

--- app.js
+++ app.js
@@ -34,4 +34,8 @@ app.get('/', routes.index);
 
 //app.listen(3000);
 var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
