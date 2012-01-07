5879ec55ed968852cae2519ff89a1172f7f72aab
Trevor Kimenye <trevor@kimenye.com>
2012-01-08 00:07:51 +0300

    Added dates and defaults

---
+++ public/javascripts/app/plan.js
@@ -0,0 +1,9 @@
+$( '#planPage' ).live( 'pageinit',function(event){
+	$('#dep_date').val(new Date.today().toString("yyyy-MM-dd"));
+	$('#ret_date').val(new Date.today().add(3).days().toString("yyyy-MM-dd"));
+});
+
+$("#frmPlan").validate({
+    submitHandler: function(form) {
+    }
+});
